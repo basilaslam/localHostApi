9c3107280397c177596c7b54aa7670167124b8bf
Basil Aslam <basilaslamnp@gmail.com>
2022-09-10 20:44:11 +0100

    Api Completed

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const routes = require('./routes/routes');
+const app = express();
+const database = mongoose.connection;
+app.use(express.json());
+
+
+app.use('/', routes)
+
+mongoose.connect(mongoString);
+
+database.on('error', (error) => {
+    console.log(error + 'err')
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
---
+++ routes/routes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const Model = require('../models/model');
+const router = express.Router()
+
+//Post Method
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        ip: req.body.ip
+       
+    })
+    console.log('ip');
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+
+//Get all Method
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get by ID API')
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete', (req, res) => {
+    Model.deleteMany({}, ()=>{
+        res.send('All deleted')
+    })
+})
+
+module.exports = router;
